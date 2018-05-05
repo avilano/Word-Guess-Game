22a4aea1bf653996817ac232deb49aa6549da3cd
Veritosvb <vero.velez@gmail.com>
2018-05-05 12:49:48 -0500

    Homework improving

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
-//Global Variable
+
 var score = 0;
 var lives ;
 var start = false;
-var toGuess = "";//random Brand to guess
-var answer =[]; //empty array
+var toGuess = "";//random Cartoon name to guess
+var answer =[]; //empty array with answer
 var messages = {
     correct: 'Correct!',
     incorrect: 'Incorrect!',
@@ -14,37 +14,54 @@ var messages = {
     instructions: 'Please press the desired key'
 }
 var availableLetters;
-var letterClicked = ["."];
-var alreadyGuessed;
+var letterClicked = ["."];//inicialiazed with . dt not able to make it work with an empty array. Keeps track of the pressed keys
+var alreadyGuessed;//counter of letters al ready guessed - Adds without typing spaces.
 var brandName = [{name:"elmer fudd", image:"47.jpeg"},{name:"buttercup", image:"46.png"},{name:"squidward tentacles", image:"45.jpeg"},
 {name:"sylvester", image:"1.jpg"},{name:"homer simpson", image:"2.jpg"},{name:"mickey mouse", image:"3.jpg"},{name:"rocky", image:"4.jpg"},{name:"wile e coyote", image:"5.jpg"},
 ,{name:"spongebob", image:"6.jpg"},{name:"eric cartman", image:"7.jpg"},{name:"daffy duck", image:"8.jpg"},{name:"porky pig", image:"9.jpg"},{name:"brian", image:"19.jpg"},{name:"betty boop", image:"12.jpg"},
 {name:"pongo", image:"32.jpg"},{name:"casper", image:"28.jpg"},{name:"bender", image:"38.jpg"},{name:"pepe le pew", image:"37.jpg"}, {name:"baloo", image:"18.jpg"},{name:"velma", image:"10.jpg"},
 {name:"optimus prime", image:"11.jpg"},{name:"ren", image:"13.jpg"}];
-var audio = new Audio('assets/audio/looney_tunes.mp3');
+var audio = new Audio('assets/audio/looney_tunes.mp3'); //when you win or when you lose sound
 
-
+/*
+ * Function called by button in hmtl. Makes the buttons hide while the user guess the image.
+ * Resets the available letters array and all other counters
+ */
 function startGame(){
-    document.getElementById("top").style.visibility= 'visible';
-    document.getElementById("outputError").innerText= messages.instructions;
     availableLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     lives = 10;
     alreadyGuessed=0;
     answer =[];
+    setElements();
     randomBrand();
     createAnswer();
     start = true;
+}
+/*
+ * default screen layout to start playing
+ * 
+ */
+function setElements(){
+    document.getElementById("top").style.visibility= 'visible';
+    document.getElementById("outputError").innerText= messages.instructions;
     document.getElementById("startGame").style.visibility = 'hidden';
     document.getElementById("title_header").style.visibility = 'hidden';
     audio.pause();
 }
-
+/*
+ * Called everytime user presses a key
+ * @event onkeyup
+ */
 document.onkeyup = function(event) {
     var keyPressed = event.key;
     if(start)
-    hangman(keyPressed.toLowerCase());
-    
+    hangman(keyPressed.toLowerCase()); 
 }
+/*
+ * Called everytime we finish sorting a letter
+ * Increases/decreses score and decides if you win/lose. Plays music.
+ */
+
 function checkScores(){
     if(alreadyGuessed == toGuess.length){
         printLives(messages.win);
@@ -63,7 +80,10 @@ function checkScores(){
     }
     document.getElementById("scoreupdate").innerText= score;
 }
-
+/**
+ * Called by checkScores. Resets the display in case of Win or Lose.
+ *
+ */
 function resetElements(){
     document.getElementById("startGame").style.visibility = 'visible';
     document.getElementById("imageGuess").style.visibility = 'hidden';
@@ -71,7 +91,10 @@ function resetElements(){
     document.getElementById("title_header").style.visibility = 'visible';
     document.getElementById("top").style.visibility= 'hidden';
 }
-
+/**
+ * main function. Recieves letter from event decides if it has been pressed
+ * Asigns the letter to a position in the screen
+ */
 function hangman(letter){
     //is the key a valid letter? 
     if (availableLetters.indexOf(letter)!==-1) {
@@ -98,7 +121,10 @@ function hangman(letter){
 
 }
 
-    //Call this function when start of new word
+/**
+ * Function which gets a random Cartoon name and photo
+ */
+
 function randomBrand(){
     var selection = Math.floor(Math.random() * brandName.length);
     toGuess = brandName[selection].name;
@@ -107,7 +133,10 @@ function randomBrand(){
     console.log(toGuess);
 }
         
-    //Call this function when start of new word
+/**
+ * Populates an empty array  with underscores to start the game
+ * Spaces are skipped
+ */
 function createAnswer(){
     for (var i = 0; i < toGuess.length; i++) {
             answer[i] = " _ ";
@@ -118,7 +147,10 @@ function createAnswer(){
     }
     printAnswer();
 }
-
+/*
+ * Function call to display changes to the array.
+ * Used by createAnswer() and hangman()
+ */
 function printAnswer(){
     var print = " ";
     for (var i = 0; i < answer.length; i++) {
@@ -127,11 +159,18 @@ function printAnswer(){
     document.getElementById("updateScreen").innerText = print;
 }
 
+/*
+ * Function call to display Lives and error messages. 
+ * Used by createAnswer() and hangman()
+ */
 function printLives(text){
     document.getElementById("outputError").innerText =text + "  - Lives: " + lives;
 }
 
-
+/*
+ * Function that searches inside the to be guessed word if the letter clicked is there
+ * @returns bool 
+ */
 function existInWord(letter){
     var result = false;
     if(toGuess.indexOf(letter)> -1){
@@ -146,7 +185,6 @@ function existInWord(letter){
     return result;
 }
 
-   //Whenever the user presses a key
 
 
 
